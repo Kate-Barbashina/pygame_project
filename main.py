3bfcc699397aa478edddb64624a593f5308560e9
Kate <Kbcatherineg100@yandex.ru>
2024-01-02 15:12:43 +0300

    Радость

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('У меня наконец-то получилось зайти!')
+    print('Всего-то 3 часа и мы это сделали')
 
 
 if __name__ == '__main__':
